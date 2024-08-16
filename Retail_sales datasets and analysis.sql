01afa8110713c08b19da10d5c3bb2c69c51b54b3
saurabh <saurabhsingh008318@gmail.com>
2024-08-16 18:36:23 +0530

    first commit

---
+++ Retail_sales datasets and analysis.sql
@@ -0,0 +1,145 @@
+
+create database retailer;
+drop table  retail_sales ;
+
+CREATE TABLE retail_sales
+(
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,	
+    sale_time TIME,
+    customer_id INT,	
+    gender VARCHAR(10),
+    age INT,
+    category VARCHAR(35),
+    quantity INT,
+    price_per_unit FLOAT,	
+    cogs FLOAT,
+    total_sale FLOAT
+);
+
+select*from retail_sales;
+
+-- Data Exploration
+
+-- How many sales we have?
+SELECT COUNT(*) as total_sale FROM retail_sales;
+
+-- How many uniuque customers we have ?
+
+SELECT COUNT(DISTINCT customer_id) as total_sale FROM retail_sales;
+
+SELECT DISTINCT category FROM retail_sales;
+
+-- Data Analysis & Business Key Problems & Answers
+
+-- My Analysis & Findings
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4.
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+-- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
+-- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+-- Q.7 Write a SQL query to calculate the total price per unit of each category.
+-- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
+-- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
+-- Q.10 Write a SQL query to find the total sales of males under the age of 30 in each category.
+
+-- ANSWERS
+
+
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+SELECT *
+FROM retail_sales
+WHERE sale_date = '2022-11-05';
+
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 .
+
+SELECT 
+  *
+FROM retail_sales
+WHERE 
+    category = 'Clothing'
+  
+    AND
+    quantity >= 4;
+
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
+
+SELECT 
+    category,
+    SUM(total_sale) as net_sale,
+    COUNT(*) as total_orders
+FROM retail_sales
+GROUP BY 1;
+
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+
+SELECT
+    ROUND(AVG(age), 2) as avg_age
+FROM retail_sales
+WHERE category = 'Beauty';
+
+
+-- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
+
+SELECT * FROM retail_sales
+WHERE total_sale > 1000;
+
+
+-- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+
+SELECT 
+    category,
+    gender,
+    COUNT(*) as total_trans
+FROM retail_sales
+GROUP 
+    BY 
+    category,
+    gender
+ORDER BY 1;
+
+
+-- Q.7 Write a SQL query to calculate the total price per unit of each category.
+
+select 
+category,
+sum(price_per_unit)
+from retail_sales
+ group by 1 ;
+    
+
+-- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
+
+SELECT 
+    customer_id,
+    SUM(total_sale) as total_sales
+FROM retail_sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
+
+-- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
+
+
+SELECT 
+    category,    
+    COUNT(DISTINCT customer_id) as cnt_unique_cs
+FROM retail_sales
+GROUP BY category;
+
+
+
+-- Q.10 Write a SQL query to find the total sales of males under the age of 30 in each category.
+
+SELECT 
+    gender, age, total_sale, category
+FROM
+    retail_sales
+WHERE
+    gender = 'Male' AND age <= '30'
+ORDER BY category , age;
+
+
+-- End of project
+
